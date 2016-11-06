0d64a694d37e0b5a3d4bb824ee5f00db8ba872d0
Michael <michaelterra@gmail.com>
2016-11-06 13:40:42 -0200

    WCFServer with Certificate Tests + Client

---
+++ WCFServer+Client/TestClient/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.ServiceClient myService = new
+                    ServiceReference1.ServiceClient();
+
+            MessageBox.Show(myService.GetData(123));
+            myService.Close();
+        }
+    }
+}
