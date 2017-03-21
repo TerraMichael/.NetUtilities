e3b0d63ef1f9716317bde94322008cdfd4c915bc
Terra <U6019906@ten.thomsonreuters.com>
2017-03-21 19:05:51 -0300

    Segundo Layout para a página

---
+++ TerraPageSolution2/TerraPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TerraPage.Startup))]
+namespace TerraPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
